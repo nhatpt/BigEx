74bb381f64e67ba4383749c2b61ada31c0d232f8
Nhat Pham Trung <37232394+nhatpt@users.noreply.github.com>
2019-05-23 23:44:24 +0700

    API Done

--- music-sys/music-sys-api/src/main/java/org/music/sys/api/Song.java
+++ music-sys/music-sys-api/src/main/java/org/music/sys/api/Song.java
@@ -2,20 +2,26 @@ package org.music.sys.api;
 
 import java.io.Serializable;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
+@Table(name = "song")
 public class Song implements Serializable {
 	private static final long serialVersionUID = 1L;
 	@Id
-//	@GeneratedValue(strategy = GenerationType.AUTO)
-	
-	@GeneratedValue
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "id")
 	private Integer id;
 	
+	@Column(name = "name")
 	private String name;
+	
+	@Column(name = "genre")
 	private String genre;
 
 	public Song(Integer id, String name, String genre) {
