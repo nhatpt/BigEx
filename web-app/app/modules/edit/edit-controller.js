3ce6f5d0fa931cf434cab5b25201bbc05d19a743
Nhat Pham Trung <37232394+nhatpt@users.noreply.github.com>
2019-06-06 09:32:47 +0700

    update constraint edit

--- web-app/app/modules/edit/edit-controller.js
+++ web-app/app/modules/edit/edit-controller.js
@@ -1,24 +1,42 @@
-app.controller('myEditCtrl', function ($scope, $http,$state, myData) {
-    $scope.song = myData.getData();
-    console.log( $scope.song);
+app.controller('myEditCtrl',
+		function($scope, $http, $state, myData, $mdDialog) {
+	
+			$scope.song = myData.getData();
+			console.log($scope.song);
 
-    $scope.cancel = function(){
-        $state.go('home');
-    }
-    
-    $scope.updateSong = function (song) {
-        var data = {
-            'name': song.name,
-            'genre': song.genre,
-            'lyrics': song.lyrics
-        };
-        console.log(data);
-        $http.put('../cxf/music/manager/system/api/' + song.id, JSON.stringify(data))
-            .then(function (response) {
-                console.log("Put Data Method Executed Successfully!");
-                $state.go('home');
-            },function (errResponse) {
-                console.log("Error while update song with id:" + song.id);
-        });
-    };
-});
+			$scope.cancel = function() {
+				$state.go('home');
+			}
+
+			$scope.updateSong = function(song, ev) {
+				console.log(song.name);
+				console.log(song.genre);
+				console.log(song.lyrics);
+				console.log($scope.checked);
+				if (song.name != '' && song.genre != '' && song.lyrics != '' && $scope.checked == true) {
+					var data = {
+						'name' : song.name,
+						'genre' : song.genre,
+						'lyrics' : song.lyrics
+					};
+
+					$http.put('../cxf/music/manager/system/api/' + song.id,
+							JSON.stringify(data)).then(
+							function(response) {
+								$state.go('home');
+							},
+							function(errResponse) {
+								console.log("Error while update song with id:"
+										+ song.id);
+							});
+				} else {
+					$mdDialog
+							.show($mdDialog.alert().clickOutsideToClose(true)
+									.title("Warning").textContent(
+											"Please fill out all fields.")
+									.ariaLabel("Alert Dialog").ok("OK")
+									.targetEvent(ev));
+				}
+
+			};
+		});
--- web-app/app/modules/services/load-service.js
+++ web-app/app/modules/services/load-service.js
@@ -1,7 +1,7 @@
 app.service("loadSong", function($http) {
     this.loadList = function() { 
-       return $http.get("http://localhost:8181/cxf/music/manager/system/api");
-        //  return $http.get("/cxf/music/manager/system/api");
+//       return $http.get("http://localhost:8181/cxf/music/manager/system/api");
+          return $http.get("/cxf/music/manager/system/api");
         // return $http.get("http://localhost:8181/cxf/mms/api");
     }
 });
